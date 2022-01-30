d6833f5a482e896296f7fde00fbff615777c62cf
BuildTools <33783503+Hubza@users.noreply.github.com>
2022-01-30 18:52:57 +0000

    add design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,6 +43,9 @@ namespace BackReplayTackler
             // textBox1
             // 
             this.textBox1.AccessibleName = "ArchiveLink";
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
             this.textBox1.Location = new System.Drawing.Point(12, 39);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(776, 20);
@@ -53,6 +56,7 @@ namespace BackReplayTackler
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
             this.label1.Location = new System.Drawing.Point(12, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(140, 24);
@@ -63,6 +67,7 @@ namespace BackReplayTackler
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.InactiveBorder;
             this.label2.Location = new System.Drawing.Point(12, 77);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(108, 24);
@@ -72,6 +77,9 @@ namespace BackReplayTackler
             // textBox2
             // 
             this.textBox2.AccessibleName = "OutputPath";
+            this.textBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Menu;
             this.textBox2.Location = new System.Drawing.Point(12, 104);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(430, 20);
@@ -80,16 +88,21 @@ namespace BackReplayTackler
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.Menu;
             this.button1.Location = new System.Drawing.Point(713, 149);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "go go go";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // progressBar1
             // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.Menu;
             this.progressBar1.Location = new System.Drawing.Point(12, 149);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(695, 23);
@@ -100,6 +113,7 @@ namespace BackReplayTackler
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label3.ForeColor = System.Drawing.SystemColors.InactiveBorder;
             this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.label3.Location = new System.Drawing.Point(13, 175);
             this.label3.Name = "label3";
@@ -114,6 +128,7 @@ namespace BackReplayTackler
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.InactiveBorder;
             this.label4.Location = new System.Drawing.Point(454, 77);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(106, 24);
@@ -123,6 +138,9 @@ namespace BackReplayTackler
             // textBox3
             // 
             this.textBox3.AccessibleName = "OutputPath";
+            this.textBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Menu;
             this.textBox3.Location = new System.Drawing.Point(458, 104);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(330, 20);
@@ -133,6 +151,7 @@ namespace BackReplayTackler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.ClientSize = new System.Drawing.Size(800, 211);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox3);
@@ -143,6 +162,7 @@ namespace BackReplayTackler
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Form1.cs
+++ Form1.cs
@@ -163,7 +163,13 @@ namespace BackReplayTackler
 
                         if (wayThrough == 9)
                         {
-                            // TODO: parse mods
+                            string[] mods = xStrip(line, "td").Split(',');
+                            int modNum = 0;
+                            foreach(string mod in mods)
+                            {
+                                // if anyone knows how to do this help, i don't.
+                            }
+                            plays[playIndex].mods = modNum;
                         }
 
                         wayThrough += 1;
@@ -207,6 +213,7 @@ namespace BackReplayTackler
             {
                 label3.Text = "Uh oh";
             }
+            label3.Text = "Done!";
         }
 
         void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
@@ -260,5 +267,53 @@ namespace BackReplayTackler
         public int mods;
         public int maxCombo;
         public string rank;
+    }
+
+    public enum Mods
+    {
+        None = 0,
+        NoFail = 1,
+        Easy = 2,
+        TouchDevice = 4,
+        Hidden = 8,
+        HardRock = 16,
+        SuddenDeath = 32,
+        DoubleTime = 64,
+        Relax = 128,
+        HalfTime = 256,
+        /// <summary>
+        /// Only set along with DoubleTime. i.e: NC only gives 576
+        /// </summary>
+        Nightcore = 512,
+        Flashlight = 1024,
+        Autoplay = 2048,
+        SpunOut = 4096,
+        /// <summary>
+        /// Autopilot
+        /// </summary>
+        Relax2 = 8192,
+        /// <summary>
+        /// Only set along with SuddenDeath. i.e: PF only gives 16416
+        /// </summary>
+        Perfect = 16384,
+        Key4 = 32768,
+        Key5 = 65536,
+        Key6 = 131072,
+        Key7 = 262144,
+        Key8 = 524288,
+        FadeIn = 1048576,
+        Random = 2097152,
+        Cinema = 4194304,
+        Target = 8388608,
+        Key9 = 16777216,
+        KeyCoop = 33554432,
+        Key1 = 67108864,
+        Key3 = 134217728,
+        Key2 = 268435456,
+        ScoreV2 = 536870912,
+        LastMod = 1073741824,
+        KeyMod = Key1 | Key2 | Key3 | Key4 | Key5 | Key6 | Key7 | Key8 | Key9 | KeyCoop,
+        FreeModAllowed = NoFail | Easy | Hidden | HardRock | SuddenDeath | Flashlight | FadeIn | Relax | Relax2 | SpunOut | KeyMod,
+        ScoreIncreaseMods = Hidden | HardRock | DoubleTime | Flashlight | FadeIn
     }
 }
